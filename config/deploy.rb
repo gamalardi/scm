87dcbeb865d258501e0bb96cfc223babfb81701a
Georgius Rinaldo <dodoxzs@hotmail.com>
2016-01-20 16:42:05 +0700

    revert

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'suryacitramedia'
-set :repo_url, 'git@github.com:scmwebdev/indosiar21v2.git'
+set :repo_url, 'https://github.com/scmwebdev/scm.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
